21e85d5c6d370c1a97deafff991c2e989abf2ee4
Vadim-BG <infotourbg.vadim@gmail.com>
2019-07-26 16:33:52 +0300

    repeat

--- src/com/javalesson/controlstatement/SwitchCase.java
+++ src/com/javalesson/controlstatement/SwitchCase.java
@@ -3,6 +3,55 @@ package com.javalesson.controlstatement;
 public class SwitchCase {
     public static void main(String[] args) {
 
+        String dayOfTheWeek = args[0];
+        switch (dayOfTheWeek.toLowerCase()) {
+            case "monday":
+                System.out.println("This is the first working day");
+                break;
+            case "tuesday":
+                System.out.println("This is the second working day");
+                break;
+            case "wednesday":
+                System.out.println("This is the third working day");
+                break;
+            case "thursday":
+                System.out.println("This is the fourth working day");
+                break;
+            case "friday":
+                System.out.println("This is the fifth working day");
+                break;
+            case "saturday": case "sunday":
+                System.out.println("Finally weekend");
+                break;
+            default:
+                System.out.println("Not a day of the week");
+
+        }
+        System.out.println("Final string");
+
+
+
+
+        /*int chois = 4;
+        switch (chois) {
+            case 1:
+                System.out.println("You chose one");
+                break;
+            case 2:
+                System.out.println("You chose two");
+                break;
+            case 3:
+                System.out.println("You chose three");
+                break;
+            case 4:
+                System.out.println("You chose four");
+                break;
+            default:
+                System.out.println("Unsupported option");
+
+        }
+        System.out.println("Final string");
+*/
 
     }
 }
