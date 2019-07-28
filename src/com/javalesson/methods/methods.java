bfd644520f06f2dfaed7519b03ca40d43fa56d3c
Vadim-BG <infotourbg.vadim@gmail.com>
2019-07-28 13:27:09 +0300

    repeat String in methods

--- src/com/javalesson/methods/methods.java
+++ src/com/javalesson/methods/methods.java
@@ -3,15 +3,30 @@ package com.javalesson.methods;
 public class methods {
     public static void main(String[] args) {
         printMessage("Alex");
+        System.out.println("Rectangle square = " + calcRectangleSquare(5, 10));
+        System.out.println("Square = " + calcSquare(5));
+        System.out.println("Sum of squares = " + (calcRectangleSquare(5, 10) + calcSquare(10)));
 
-        int a = 5;
-        int b = 10;
-        //int rectangleSquare = calcRectangleSquare(a, b);
-        //int square = calcSquare(b);
-        System.out.println("Rectangle square = " + calcRectangleSquare(a, b));
-        System.out.println("Square = " + calcSquare(b));
-        //int sumOfSquares = rectangleSquare + square;
-        System.out.println("Sum of squares = " + (calcRectangleSquare(a, b)+calcSquare(b)));
+        String str1 = "I like black coffee";
+        String str3 = "I like coffee!!!";
+        System.out.println(str1.toUpperCase());
+        System.out.println(str1.toLowerCase());
+
+        String str2 = "I like coffee";
+        String str4 = new String("I like coffee");
+        String substring = str3.substring(0, 13);
+        System.out.println("Substring = "+substring);
+//        boolean b = str2.equals(substring);
+//        System.out.println(b);
+
+        boolean like = str1.endsWith("fee!");
+        System.out.println(like);
+
+        System.out.println(str1.replace("black","white"));
+
+        System.out.println(str3.indexOf("!!!"));
+
+
     }
 
     static void printMessage(String name) {
