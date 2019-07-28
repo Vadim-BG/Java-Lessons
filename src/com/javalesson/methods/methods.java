a47371dc0cbe7ebd88f374afcbb4932bce658c0c
Vadim-BG <infotourbg.vadim@gmail.com>
2019-07-28 12:26:30 +0300

    repeat

--- src/com/javalesson/methods/methods.java
+++ src/com/javalesson/methods/methods.java
@@ -2,7 +2,29 @@ package com.javalesson.methods;
 
 public class methods {
     public static void main(String[] args) {
+        printMessage("Alex");
 
+        int a = 5;
+        int b = 10;
+        //int rectangleSquare = calcRectangleSquare(a, b);
+        //int square = calcSquare(b);
+        System.out.println("Rectangle square = " + calcRectangleSquare(a, b));
+        System.out.println("Square = " + calcSquare(b));
+        //int sumOfSquares = rectangleSquare + square;
+        System.out.println("Sum of squares = " + (calcRectangleSquare(a, b)+calcSquare(b)));
+    }
 
+    static void printMessage(String name) {
+        System.out.println("Hello " + name + "!!!");
+    }
+
+    static int calcRectangleSquare(int x, int y) {
+        int square = x * y;
+        return square;
+
+    }
+
+    static int calcSquare(int x) {
+        return x * x;
     }
 }
